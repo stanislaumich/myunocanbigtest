c68d9c1a65602e506f3a7af8c02659c89bf14f4f
stanislaumich <staslz42stas@gmail.com>
2021-04-27 12:38:48 +0300

    First work start and work

--- src/main.cpp
+++ src/main.cpp
@@ -3,66 +3,27 @@
 
 // https://www.drive2.ru/l/485801071864709696/
 
-/*
- * demo: CAN-BUS Shield, send random id/type/data
- * This code has a little same with linux can-utils 'cangen -v'
- *
- * Copyright (C) 2020 Seeed Technology Co.,Ltd.
- */
+
+// https://www.drive2.ru/c/516749850285442175/
+
+//https://www.drive2.ru/c/486832413771563406/
+
+
 #include <SPI.h>
 
 #define CAN_2515
-// #define CAN_2518FD
 
-// Set SPI CS Pin according to your hardware
-
-#if defined(SEEED_WIO_TERMINAL) && defined(CAN_2518FD)
-// For Wio Terminal w/ MCP2518FD RPi Hat：
-// Channel 0 SPI_CS Pin: BCM 8
-// Channel 1 SPI_CS Pin: BCM 7
-// Interupt Pin: BCM25
-const int SPI_CS_PIN  = BCM8;
-const int CAN_INT_PIN = BCM25;
-#else
-
-// For Arduino MCP2515 Hat:
-// the cs pin of the version after v1.1 is default to D9
-// v0.9b and v1.0 is default D10
 const int SPI_CS_PIN = 10;
 const int CAN_INT_PIN = 2;
-#endif
 
-
-#ifdef CAN_2518FD
-#include "mcp2518fd_can.h"
-mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
-
-// TEST TEST MCP2518FD CAN2.0 data transfer
-#define MAX_DATA_SIZE 8
-// To TEST MCP2518FD CANFD data transfer, uncomment below lines
-// #undef  MAX_DATA_SIZE
-// #define MAX_DATA_SIZE 64
-
-#endif//CAN_2518FD
-
-#ifdef CAN_2515
 #include "mcp2515_can.h"
 mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
 #define MAX_DATA_SIZE 8
-#endif
+
 
 void setup() {
     SERIAL_PORT_MONITOR.begin(9600);
     while (!SERIAL_PORT_MONITOR) {}
-
-    #if MAX_DATA_SIZE > 8
-    /*
-     * To compatible with MCP2515 API,
-     * default mode is CAN_CLASSIC_MODE
-     * Now set to CANFD mode.
-     */
-    CAN.setMode(CAN_NORMAL_MODE);
-    #endif
     while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
         SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
         delay(100);
@@ -100,20 +61,10 @@ void loop() {
     for (i = 0; i < len; i++) {
         cdata[i] = random(0x100);
     }
-    #if MAX_DATA_SIZE > 8
-    // pad CANFD extra bytes with 0
-    for (i = len; i < MAX_DATA_SIZE; i++) {
-        cdata[i] = 0;
-    }
-    #endif
 
     CAN.sendMsgBuf(id, bool(type & 0x1),
                        bool(type & 0x2),
-                     #if MAX_DATA_SIZE > 8
-                       CANFD::len2dlc(len),
-                     #else
                        len,
-                     #endif
                        cdata);
 
     char prbuf[32 + MAX_DATA_SIZE * 3];
@@ -135,7 +86,7 @@ void loop() {
     }
     SERIAL_PORT_MONITOR.println(prbuf);
 
-    unsigned d = random(30);
+    unsigned d = random(50);
     SERIAL_PORT_MONITOR.println(d);
     delay(d);
 }
