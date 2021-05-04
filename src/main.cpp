7f7f03e6b478ae40d253a28e47a24a9890c43d44
stanislaumich <staslz42stas@gmail.com>
2021-05-04 14:16:20 +0300

    One shot getted!@!!

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,45 @@
 
 //https://en.wikipedia.org/wiki/OBD-II_PIDs#Mode_01
 
+
+// CAN Send Example
+//
+
+#include <mcp_can.h>
+#include <SPI.h>
+
+MCP_CAN CAN0(10);     // Set CS to pin 10
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
+  else Serial.println("Error Initializing MCP2515...");
+
+  CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+  CAN0.enOneShotTX();
+}
+
+byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+
+void loop()
+{
+  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
+  if(sndStat == CAN_OK){
+    Serial.println("Message Sent Successfully!");
+  } else {
+    Serial.println("Error Sending Message...");
+  }
+  delay(2000);   // send data per 100ms
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/ 
+/*
 #include <SPI.h>
 #define CAN_2515
 
@@ -37,6 +76,7 @@ void setup() {
         SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
         delay(100);
     }
+    //enOneShotTX();
     SERIAL_PORT_MONITOR.println("CAN init ok!");
 }
 
@@ -45,7 +85,8 @@ void loop() {
     // send data:  id = 0xDF, standrad frame, data len = 8, stmp: data buf
  
     CAN.sendMsgBuf(0xDF, 0, 8, stmp);
+//false   1
     delay(2000);                       // send data per 2000ms
     SERIAL_PORT_MONITOR.println("CAN BUS sendMsgBuf ok!");
     SERIAL_PORT_MONITOR.println(stmp[7]);
-}
+}*/
